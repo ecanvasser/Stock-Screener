e3788b43f7ce568b832545d3c84667b0d4102899
ecanvasser <eddiecanvassere9@gmail.com>
2020-04-28 17:07:25 -0700

    Removed useless code

--- stockscore.py
+++ stockscore.py
@@ -10,8 +10,9 @@ class Stock:
         today = date.today().strftime('%m%d%Y')
         final_stats = []
         for arg in args:
+            # Gets JSON article data for ticker symbol. Make sure to input API key
             sentiment_response = requests.get('https://stocknewsapi.com/api/v1?tickers={}&items=15&date={}-{}&sortby=unique&token='.format(arg,today,today)).json()
-
+            # Gets JSON stock price data. Make sure to input API key
             price_response = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={}&interval=1min&apikey='.format(arg)).json()
             prices_json = list(price_response['Time Series (1min)'].values())
 
@@ -29,7 +30,7 @@ class Stock:
                     else:
                         pass
             
-            # Cleans text_tick list by removing all repeated values. Only saves the unique values
+            # Cleans text_tick by removing all repeated values. Only saves the unique values
             cleaned_text = []            
             for items in text_tick:
                 if items not in cleaned_text:
@@ -43,14 +44,6 @@ class Stock:
                     ticks_freq[ticks] += 1
                 else:
                     ticks_freq[ticks] = 1
-
-            # Checks if all arguments have sentiment score. If not, passes text response
-            keys = list(ticks_freq.keys())
-            for t in args:
-                if t in keys:
-                    pass
-                else:
-                    ticks_freq[t] = 'No news found'
             
             scores = {}
             raw_scores = []
@@ -63,6 +56,7 @@ class Stock:
                         tblob = TextBlob(txt)
                         pol = round(tblob.sentiment.polarity, 2)
                         raw_scores.append([pol,a])
+            
             # Calculates final score
             total_score = 0
             for b in raw_scores:
